e9d6b60aa98773584e480bd7c294b93ce3f43cf4
Fabio Ferreira <fabio.ferreira3@outlook.com>
2013-12-02 23:02:36 -0200

    correcao busca de produto por preco e estoque

--- application/models/mappers/Product.php
+++ application/models/mappers/Product.php
@@ -668,8 +668,10 @@ class DB_Product
     
         $query = $em->createQueryBuilder();        
         $query->select('p')->from('DB_Product','p');
+       
         if($params['status'] != ''){
             $query->where('p.status = :status');
+            
             $query->setParameter('status',$params['status']);            
         }else{
             $query->where('p.status != 1234567');            
@@ -682,32 +684,28 @@ class DB_Product
             $query->andWhere('p.sku LIKE :sku');
             $query->setParameter('sku',"%".$params['sku']."%");            
         } 
-        if(isset($params['priceFrom']) && $params['priceFrom'] && !isset($params['priceTo'])){        	
+        if(isset($params['priceFrom']) && $params['priceTo'] && $params['priceTo'] != '' && $params['priceFrom'] != ''){
+        	$query->innerjoin('p.price','pr','WITH','pr.price <= :priceTo AND pr.price >= :priceFrom');
+        	$query->setParameters(array('priceFrom' => $params['priceFrom'], 'priceTo' => $params['priceTo']));
+        	
+        }else if(isset($params['priceFrom']) && $params['priceFrom']){        	
         	$query->innerjoin('p.price','pr','WITH','pr.price >= :priceFrom');
         	$query->setParameter('priceFrom',$params['priceFrom']);     
-        }
-        if(isset($params['priceTo']) && $params['priceTo'] && !isset($params['priceFrom'])){
+        }else if(isset($params['priceTo']) && $params['priceTo']){
         	$query->innerjoin('p.price','pr','WITH','pr.price <= :priceTo');
         	$query->setParameter('priceTo',$params['priceTo']);
-        }
-        if(isset($params['priceFrom']) && $params['priceFrom'] && isset($params['priceTo']) && $params['priceTo']){
-         	$query->innerjoin('p.price','pr','WITH','pr.price <= :priceTo AND pr.price >= :priceFrom');
-         	$query->setParameter('priceFrom',$params['priceFrom']);
-         	$query->setParameter('priceTo',$params['priceTo']);
-        }
-        if(isset($params['stockFrom']) && $params['stockFrom'] && !isset($params['stockTo'])){
-        	$query->innerjoin('p.stock','st','WITH','st.currentQty >= :stockFrom');
-        	$query->setParameter('stockFrom',$params['stockFrom']);
-        }
-        if(isset($params['stockTo']) && $params['stockTo'] && !isset($params['stockFrom'])){
-        	$query->innerjoin('p.stock','st','WITH','st.currentQty <= :stockTo');
-        	$query->setParameter('stockTo',$params['stockTo']);
-        }
-        if(isset($params['stockFrom']) && $params['stockFrom'] && isset($params['stockTo']) && $params['stockTo']){
-        	$query->innerjoin('p.stock','st','WITH','st.currentQty <= :stockTo AND st.currentQty >= :stockFrom');
-        	$query->setParameter('stockFrom',$params['stockFrom']);
-        	$query->setParameter('stockTo',$params['stockTo']);
-        }
+        }        
+       
+        if(isset($params['stockFrom']) && isset($params['stockTo']) && $params['stockTo'] != '' && $params['stockFrom'] != ''){
+           	$query->innerjoin('p.stock','st','WITH','st.currentQty <= :stockTo AND st.currentQty >= :stockFrom');
+        	$query->setParameters(array('stockFrom' => $params['stockFrom'], 'stockTo' => $params['stockTo']));
+        }else if(isset($params['stockFrom']) && $params['stockFrom']){
+        	$query->innerjoin('p.stock','st','WITH','st.currentQty >= :stockFrom');       
+        	$query->setParameter('stockFrom',$params['stockFrom']);      	
+        }else if(isset($params['stockTo']) && $params['stockTo']){
+        	$query->innerjoin('p.stock','st','WITH','st.currentQty <= :stockTo'); 
+        	$query->setParameter('stockTo',$params['stockTo']);        	
+        }       
         
         $query->orderBy('p.id','DESC');
         
@@ -847,8 +845,13 @@ class DB_Product
     	                     if(!isset($conditions['noprice'])){
     	                     	$html.= "<td>" . $price . "</td>";
     	                     }
-    	                     $html.= "<td>" . $row->getCurrentQty() . "</td>";
-    	                     
+    	                     if(!isset($conditions['nostock'])){
+    	                     	if(isset($conditions['editStock'])){
+    	                     		$html.= "<td colspan='2'><input type='text' value='" . $row->getCurrentQty() . "' class='editStock span4'/></td>";
+    	                     	}else{
+    	                     		$html.= "<td colspan='2'>" . $row->getCurrentQty() . "</td>";
+    	                     	}    	                     	
+    	                     }
     	                     // Mostra ou oculta o status do produto
     	                     if(!isset($conditions['nostatus'])){
     	                     	$html.= "<td>" . $status . "</td>";
--- application/modules/admin/controllers/ProductController.php
+++ application/modules/admin/controllers/ProductController.php
@@ -443,6 +443,7 @@ class Admin_ProductController extends Zend_Controller_Action
             if($params['noactions']){ $conditions['noactions'] = true; }
             if($params['inputQty']){ $conditions['inputQty'] = true; }
             if($params['addButton']){ $conditions['addButton'] = true; }
+            if($params['noprice']){ $conditions['noprice'] = true; }
             
             $results = $tbProduct->getProductsByFilter($params);
            // Zend_Debug::dump($results);exit;
@@ -470,6 +471,7 @@ class Admin_ProductController extends Zend_Controller_Action
     	$conditions['noprice'] = true;
     	$conditions['nostatus'] = true;
     	$conditions['noactions'] = true;
+    	$conditions['editStock'] = true;
     	$this->view->products = $tbProducts->generateTable($tbProducts->getProducts(),$conditions);
     }
 }
